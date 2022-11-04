279e0dfe6c26e968fa24c902c9ec5be5c4700d6c
unknown <saurab.rawat@jamsfy.com>
2022-11-04 10:56:24 +0530

    changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 
   app.on("issues.opened", async (context) => {
     const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+      body: "Thanks for opening this issue!.",
     });
     return context.octokit.issues.createComment(issueComment);
   });
